b53f196a5e79a73fde7aa43a19fc6eaad41de675
Mauricio Cesar Rojas <78119017+MauricioCRojas@users.noreply.github.com>
2021-03-03 20:54:52 -0300

    programacion 3-3-2020 ingreso parcial

--- dos.js
+++ dos.js
@@ -1,4 +1,160 @@
+/*
+Enunciado:
+
+Realizar el algoritmo que permita ingresar los datos de los alumnos de una 
+division de la UTN FRA, los datos solicitados son:
+nombre
+situcación laboral ("buscando" , "trabajando" o "solo estudiante")
+cantidad de materias( mas de cero y menos de 8)
+Sexo ( femenino , masculino , no binario)
+Nota promedio (entre 0 y 10)
+edad (validar)
+se debe informar de existir, o informar que no existe , en el caso que corresponda.
+a) El nombre del mejor promedio que no de los que trabajan o estan buscando
+b) El nombre del mas viejo de los alumnos que solo sea estudiantes
+c) El promedio de nota por situacion laboral
+d) La edad y nombre del que cursa menos cantidad de materias y que este buscando trabajo
+
+*/
+
+
 function mostrar()
 {
-  alert("dos");
+
+  let nombre;
+  let situacionLaboral;
+  let cantidadMaterias;
+  let sexo;
+  let notaPromedio;
+  let edad;
+
+  let mejorPromedioSE=0;
+  let nombreMPSE;
+  let flagNMPSE=0;
+  let seguir;
+
+  let flagMVSE=0;
+  let edadMVSE;
+  let nombreMVSE;
+
+  let contT=0;
+  let contB=0;
+  let contSE=0;
+  let acumT=0;
+  let acumB=0;
+  let acumSE=0;
+  let promedioT=0;
+  let promedioB=0;
+  let promedioSE=0;
+
+  let flagMenosCM=0;
+  let menosCM;
+  let edadMenosCM;
+  let nombreMenosCM;
+
+
+do{
+  nombre= prompt("Ingrese su nombre");
+
+  situacionLaboral = prompt("Ingrese su situacion laboral buscando/trabajando/solo estudiante");
+  while (!(situacionLaboral=="buscando" || situacionLaboral== "trabajando" || situacionLaboral=="solo estudiante")){
+ situacionLaboral = prompt("Error. Ingrese su situacion laboral buscando/trabajando/solo estudiante");
+  }
+   
+  cantidadMaterias= parseInt(prompt("Ingrese cantidad de materias entre 1 y 7"));
+  while(isNaN (cantidadMaterias) || cantidadMaterias <=0 || cantidadMaterias >7){
+  cantidadMaterias= parseInt(prompt("Error. Ingrese cantidad de materias entre 1 y 7"));
+  }
+ 
+  sexo=prompt("Ingrese su sexo f/m/nb");
+  while (!(sexo=='f' || sexo=='m' || sexo=="nb")){
+  sexo=prompt("Error. Ingrese su sexo f/m/nb");   
+  }
+ 
+  notaPromedio=parseFloat(prompt("Ingrese su promedio de notas"));
+  while (isNaN(notaPromedio) || notaPromedio >10 || notaPromedio<=0){
+  notaPromedio=parseFloat(prompt("Error. Ingrese su promedio de notas"));
+  }
+ 
+ 
+  edad = parseInt(prompt("Ingrese su edad"));
+  while (isNaN(edad)){
+    edad = parseInt(prompt("Error. Ingrese su edad"));
+    
+  }
+  
+  
+  //A
+  if (situacionLaboral=="solo estudiante" && (flagNMPSE== 0 || notaPromedio >mejorPromedioSE)){
+    nombreMPSE=nombre;
+    mejorPromedioSE= notaPromedio;
+    flagNMPSE=1;
+  }
+  
+  
+  if (situacionLaboral=="solo estudiante" && (flagMVSE==0 || edad > edadMVSE)){//B
+    nombreMVSE= nombre;
+    edadMVSE=edad;
+    flagMVSE=1;
+  }
+  
+  
+  switch(situacionLaboral){
+    case "solo estudiante":
+      contSE++;
+      acumSE+= notaPromedio;
+      break;
+      case "buscando":
+        contB++;
+        acumB+= notaPromedio;
+        break;
+        case "trabajando":
+          contT++;
+          acumT+=notaPromedio;
+          break;
+        }
+        
+        if (situacionLaboral=="buscando" && (flagMenosCM==0 || cantidadMaterias < menosCM)){//D
+          menosCM=cantidadMaterias;
+          edadMenosCM=edad;
+          nombreMenosCM=nombre;
+          flagMenosCM=1;
+        }
+        seguir=prompt("¿Quiere ingresar otro dato s/n");
+      }while(seguir=='s');
+      
+      
+      //A y //B
+      if (mejorPromedioSE!=0){
+        console.log ("El nombre del mejor promedio de solo estudiantes es " + nombreMPSE);
+  console.log ("El nombre del mas viejo de los alumnos solo estudiantes "+ nombreMVSE);
+}
+else {
+  console.log ("No se ingresaron alumnos solo estudiantes");
+}
+
+//C
+if (contT !=0){
+  promedioT= acumT/contT;
+  console.log ("El promedio de notas de trabajadores es " + promedioT);
+}
+else {
+  console.log ("No se ingresaron alumnos trabajadores");
+}
+if (contB !=0){
+  promedioB= acumB/contB;
+  console.log ("El promedio de notas de alumnos buscando trabajo es " + promedioB);
+}
+else {
+  console.log ("No se ingresaron alumnos buscando trabajo");
+}
+if (contSE !=0){
+  promedioSE= acumSE/contSE;
+  console.log ("El promedio de notas de solo estudiantes es " + promedioSE);
+}
+
+//D
+if (flagMenosCM !=0){
+  console.log ("El nombre de quien cursa menos materias y busca trabajo es " + nombreMenosCM + " y su edad es " + edadMenosCM);
+}
 }
--- uno.js
+++ uno.js
@@ -1,5 +1,152 @@
+/*
+Enunciado:
 
+Debemos realizar la carga de una compra de 5(cinco) productos de desinfección
+de cada una debo obtener los siguientes datos:
+el nombre del producto el tipo de producto (validar "ALCOHOL", "IAC" o "QUAT"),
+el precio (validar entre 100 y 300),
+la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades),
+Categoria ("desinfectante", "bactericida", "detergente" ) y el fabricante.
+Se debe Informar al usuario lo siguiente:
+a) El promedio de cantidad por tipo de producto
+b) La categoria con mas cantidad de unidades en total de la compra
+c) Cuántas unidades de detergente con precios menos a 200 (inclusive) se compraron en total
+d) el fabricante y Categoria del más caro de los productos
+*/
 function mostrar()
 {
-	alert("uno");
+
+	let producto;
+	let precio;
+	let unidades;
+	let categoria;
+
+	let acumAlcohol=0;
+	let acumIac=0;
+	let acumQuat=0;
+	let contAlcohol=0;
+	let contIac=0;
+	let contQuat=0;
+	let promedioQuat=0;
+	let promedioIac=0;
+	let promedioAlcohol=0;
+	let flag=0;
+	let precioTipoMasCaro;
+	let fabricanteTipoMasCaro;
+	let categoriaMasCaro;
+	let acumDesinfectante=0;
+	let acumBactericida=0;
+	let acumDetergente=0;
+	let acumDetergenteBarato=0;
+
+
+
+	for ( let i = 1 ; i <=2 ; i++){
+	producto = prompt("Ingrese producto a comprar: ALCOHOL/IAC/QUAT");
+	while ( !(producto == "ALCOHOL" || producto == "IAC" || producto == "QUAT")){
+		producto = prompt("Error. Ingrese producto a comprar:  ALCOHOL/IAC/QUAT");
+	}
+	precio = parseFloat (prompt("Ingrese precio"));
+	while (isNaN(precio) || precio < 100 || precio > 300){
+		precio = parseFloat (prompt("Error.Ingrese precio"));
+	}
+	unidades = parseInt (prompt("Ingrese cantidad de unidades"));
+	while (isNaN(unidades) || unidades <= 0 || unidades > 1000){
+		unidades = parseInt (prompt("Error. Ingrese cantidad de unidades"));
+	}
+	categoria= prompt("Ingrese categoria desinfectante/bactericida/detergente");
+	while (!(categoria == "desinfectante" || categoria == "bactericida" || categoria == "detergente")){
+	categoria= prompt("Error. Ingrese categoria desinfectante/bactericida/detergente");
+	}
+	fabricante= prompt("Ingrese fabricante");
+
+	switch (producto){
+		case "ALCOHOL"://A
+			acumAlcohol+= unidades;
+			contAlcohol++
+			break;
+	
+		case "IAC":
+			acumIac+= unidades;
+			contIac++
+			break;
+		case "QUAT":
+			acumQuat+= unidades;
+			contQuat++
+			break;		
+	}
+
+	switch(categoria){//B
+		case "bactericida":
+			acumBactericida+=unidades;
+			break;
+		case "desinfectante":
+			acumDesinfectante+=unidades;
+			break;
+		case "detergente":
+			acumDetergente+=unidades;
+			if (precio <= 200){//C
+				acumDetergenteBarato++;
+			}
+			break;
+
+
+
+	}
+	if (flag==0 || precio > precioTipoMasCaro){//D
+		precioTipoMasCaro=precio;
+		categoriaMasCaro = categoria;
+		fabricanteTipoMasCaro = fabricante;
+		flag =1;
+		}
+	}
+
+
+
+
+	if (contQuat!=0){//A
+		promedioQuat= acumQuat / contQuat;
+		console.log ("El promedio por compra de quat es: " + promedioQuat);
+	}
+	else {
+		console.log ("No se ingresaron productos quat");
+	}
+	if (contIac!=0){//A
+		promedioIac= acumIac / contIac;
+		console.log ("El promedio por compra de Iac es: " + promedioIac);
+	}
+	else {
+		console.log ("No se ingresaron productos iac");
+	}
+	if (contAlcohol!=0){//A
+		promedioAlcohol= acumAlcohol / contAlcohol;
+		console.log ("El promedio por compra de Alcohol es: " + promedioAlcohol);
+	}
+	else {
+		console.log ("No se ingresaron productos Alcohol");
+	}
+
+
+
+	if (acumBactericida > acumDesinfectante && acumBactericida > acumDetergente){//B
+		console.log ("La categoria con mas unidades en la compra es Bactericida");
+		}
+		else if (acumDesinfectante >= acumBactericida && acumDesinfectante > acumDetergente){
+			console.log ("La categoria con mas unidades en la compra es Desinfectante ");
+		}
+		else {
+			console.log ("La categoria con mas unidades en la compra es Detergente");
+		}
+
+		
+		if (acumDetergenteBarato!=0){//C
+			console.log ("Se compraron de detergentes menor o igual a 200$ la cantidad de " + acumDetergenteBarato);
+		}
+		else {
+			console.log ("No se compro detergente menor o igual a 200$");
+		}
+
+
+		//D
+console.log ("La categoria del mas caro es " +categoriaMasCaro+" y su fabricante es " + fabricanteTipoMasCaro);
 }
